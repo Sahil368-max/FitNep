33e4f7d8936d018cf3a59c1e114340da2b532631
Your Name <you@example.co m>
2026-06-30 10:46:18 +0545

    Initial commit

---
+++ app/src/main/java/com/example/fitnep/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.fitnep.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.fitnep.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
