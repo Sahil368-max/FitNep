2cde9e3f3a55b23bd1ed38d3543f5782256ae94a
Your Name <you@example.co m>
2026-07-10 12:39:51 +0545

    define base user data model for authentication

---
+++ app/src/main/java/com/example/fitnep/data/model/User.kt
@@ -0,0 +1,10 @@
+package com.example.fitnep.data.model
+
+data class User(
+    val uid: String = "",
+    val name: String = "",
+    val email: String = "",
+    val age: Int = 0,
+    val height: Double = 0.0,
+    val weight: Double = 0.0
+)
